1a7845238c1ac02eafa81ec0d4a41190e6faad48
kaelfemboy <felipesiqueiraxav@gmail.com>
2024-10-14 21:49:50 -0300

    aaaaaaaa

--- source/tcp/term.py
+++ source/tcp/term.py
@@ -1,12 +1,20 @@
-import configparser 
+import toml
 #from start import *
 
 print("Welcome to Tubo, terminal of NVTBT Videos in version 0.0.1! >//<\nTo know you Host, send 'ifconfig' in you terminal.\nTo help for commands, send help.")
 notConnected = "Not connected"
 
+checkConnection = "Not Connected" 
+# Configurations of the toml
+
+with open('config.toml', 'r') as f:
+	config = toml.load(f)
+
+print(config['network']['host'])
+# Terminal Logic
 while True:
 
-    terminal = input("tubo > ")
+    terminal = input("tubo ({})> ".format(checkConnection))
 
     parameter = terminal.split()
 
@@ -36,14 +44,18 @@ while True:
                 match key:
                     case "host":
                         print("Host set at {}".format(value))
+                        config['network']['host'] = value
                     case "port":
                         print("Port set at {}".format(value))
-
+                        config['network']['port'] = value
                     case _:
                         print("Unknown parameter")
             else:
                 print("Error value")
-                
+
+            with open('config.toml', 'w') as f:
+               toml.dump(config, f)
+ 
         case _:
             print("Unknown command.".format(command))
 
