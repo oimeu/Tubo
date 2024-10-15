87b16714e1857f0fd1a4f3d3fe51996e97348598
kaelfemboy <felipesiqueiraxav@gmail.com>
2024-10-15 09:25:13 -0300

    eu consegui

--- source/tcp/start.py
+++ source/tcp/start.py
@@ -2,5 +2,14 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind()
+server.bind(('192.168.0.104', 19200))
+server.listen(1)
 
+connection, address = server.accept()
+namefile = connection.recv(1024).decode()
+
+with open(namefile, 'rb') as file:
+    for data in file.readlines():
+        connection.send(data)
+
+    print('Arquivo enviado')
